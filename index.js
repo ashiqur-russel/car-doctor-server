12e4f28b6225042907d24bce51b903b68fd4479d
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-02 01:21:27 +0100

    mongodb connection setup

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}}:${process.env.DB_PASSWORD}@cluster0.irpitar.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get("/", (req, res) => {
   res.send("Car Doctor server is running");

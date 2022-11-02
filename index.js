f3c56e9ec7be077158cbe2332cea1ade803e506b
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-02 21:52:51 +0100

    Get data from db by specipux id

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { RssFeedRounded } = require("@material-ui/icons");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.irpitar.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,11 +22,22 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("carServicea").collection("services");
+    const orderCollection = client.db("carServicea").collection("orders");
+    //get all data from db
+
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    //get element by id
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
     });
   } finally {
   }

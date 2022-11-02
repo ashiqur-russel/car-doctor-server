206a450cd067cfecdeca9f8ff9e17bcc20713095
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-02 16:33:44 +0100

    get data from mongodb

--- index.js
+++ index.js
@@ -10,13 +10,28 @@ app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.DB_USER}}:${process.env.DB_PASSWORD}@cluster0.irpitar.mongodb.net/?retryWrites=true&w=majority`;
+const { RssFeedRounded } = require("@material-ui/icons");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.irpitar.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    const serviceCollection = client.db("carServicea").collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 app.get("/", (req, res) => {
   res.send("Car Doctor server is running");
 });

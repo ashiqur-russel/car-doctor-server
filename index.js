4068e028b607a579b0e2624e7187bc4451758403
ashiqurras <ashiqurrussel2@gmail.com>
2022-11-03 01:29:40 +0100

    Update Order status API created

--- index.js
+++ index.js
@@ -68,6 +68,20 @@ async function run() {
       const result = await orderCollection.deleteOne(query);
       res.send(result);
     });
+    //Update doc with status
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+      const query = { _id: ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      const result = await orderCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
   } finally {
   }
 }
